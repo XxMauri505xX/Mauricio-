da0d1cbe7ed7de0dbd246050959680fb27f85399
XxMauri505xX <47698329+XxMauri505xX@users.noreply.github.com>
2019-04-26 01:22:00 -0500

    Add files via upload

---
+++ Practica#5.sql
@@ -0,0 +1,147 @@
+Select * from  AlmacenCocina
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (100, 'Cocina Moderna', '50000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (101, 'Cocina Rustica', '40000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (102, 'Cocina Furtiva', '55000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (103, 'Cocina Barata', '20000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (104, 'Cocina Futuristica', '70000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (105, 'Cocina Granito', '100000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (106, 'Cocina 50S', '5500','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (107, 'Cocina Clasica', '50000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (108, 'Cocina Colorida', '55000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (109, 'Cocina Metalica', '60000','blanca')
+insert into AlmacenCocina (id_Cocina,nombreCocina,costo,especificaciones)values (110, 'Cocina Rustica', '10000','blanca')
+
+Select * from AlmacenComedor 
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (200, 'Comedor Madera','35000','Caoba')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (201, 'Comedor Madera','60000','Roble')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (202, 'Comedor Madera','45000','Adobe')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (203, 'Comedor Madera','38000','Cedro')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (204, 'Comedor Madera','37000','Abedul')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (205, 'Comedor Madera','32000','Nogal')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (206, 'Comedor Madera','45000','Teca')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (207, 'Comedor Clasico','65000','Negro')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (208, 'Comedor Moderno','75000','Blanco')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (209, 'Comedor Unico','85000','Azul')
+insert into AlmacenComedor (id_comedor,nombreComedor,costo,especificaciones)values (210, 'Comedor Futurista','95000','Crema')
+
+Select * from AlmacenElectrodomesticos
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (300, 'Microhondas','2000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (301, 'Refrigerador','10000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (302, 'Lavadora','7000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (303, 'Secadora','11000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (304, 'Licuadora','2000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (305, 'Plancha','1000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (306, 'Batidora','800','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (307, 'Cafetera','850','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (308, 'Estufa','8500','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (309, 'Congelador','2000','Negro')
+insert into AlmacenElectrodomesticos(Id_Electrodomestico,nombreElectrodomestico,costo,especificaciones)values (310, 'Tostadora','2000','Negro')
+
+Select * from AlmacenRecamaras
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (400, 'Infantil','35000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (401, 'Deparatamental','40000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (402, 'Clasica','45000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (403, 'Antigua','30000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (404, 'Veraniega','36000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (405, 'Moderna','50000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (406, 'Futuristica','70000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (407, 'Madera','25000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (408, 'Rustica','26000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (409, 'Furtiva','15000','Todos colores')
+insert into AlmacenRecamaras(Id_Recamara,nombreRecamara,costo,especificaciones)values (410, 'Reyno','100000','Todos colores')
+
+Select * from AlmacenSalas 
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (500, 'Infantil','35000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (501, 'Deparatamental','40000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (502, 'Clasica','45000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (503, 'Antigua','30000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (504, 'Veraniega','36000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (505, 'AM','50000','NEGRO')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (506, 'Futuristica','70000','Todos colores') 
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (507, 'Madera','25000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (508, 'Rustica','26000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (509, 'Furtiva','15000','Todos colores')
+insert into AlmacenSalas(Id_Salas,nombreSalas,costo,especificaciones)values (510, 'Reyno','100000','Todos colores')
+
+Select * from Empleados 
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(600,'Alejandro','Batres','Herrera','9/12/1998','81125465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(601,'Mauricio','Gutierrez','Herrera','3/12/1999','81125465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(602,'Alan','Garcia','Valdez','9/11/1998','81125485')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(603,'Alfonso','Garcia','Ulloa','2/2/1998','83125465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(604,'Juan','Perez','Herrera','9/4/1998','81175465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(605,'Arina','Lopez','Guzman','8/6/1998','81129465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(606,'Sofia','Perez','Herrera','9/12/1998','81125865')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(607,'Alejandro','Olivo','Juarez','30/12/1998','81128765')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(608,'Elma','Ma','Don','3/12/1998','81165465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(609,'Zacarias','Flores','Del campo','14/12/1998','81167465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(610,'Alejandra','Guzman','Herrera','11/06/1998','81145465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(611,'Benito','Armendariz','Cebolla','9/11/1998','81176465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(612,'Roberto','Luna','Cruz','4/8/1998','81129865')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(613,'Fernando','Lona','Lona','8/10/1998','81127865')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(614,'Fernanda','Almaguer','Dtus','9/9/1998','81125465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(615,'Elva','Castillo','Jinon','11/11/2001','81125765')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(616,'Lucas','Gutierrez','Herrera','8/12/1998','81125665')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(617,'Miranda','Alatorre','Herrera','1/1/1998','81125565')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(618,'Cristiano','Ronaldo','Du aveiro','6/1/1998','81125465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(619,'Alejandra','Batres','Bolaños','8/1/1998','81176465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(620,'Lionel','Pecho','Frio','9/2/1998','81128465')
+insert into Empleados(Id_Empleado,nombre,ApPaterno,ApMaterno,fechanacimiento,telefono)values(621,'Frida','Perez','De la Rosa','6/12/1998','81128865')
+
+Select * from Sucursal
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (700,'MuebleriaFCFM1','Casa bella','Casa de juarez','505' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (701 ,'MuebleriaFCFM2','Casa bella','Casa de juarez','510' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (702,'MuebleriaFCFM3','Casa bella','Casa de juarez','520' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (703,'MuebleriaFCFM4','Casa bella','Casa de juarez','530' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (704,'MuebleriaFCFM5','Casa bella','Casa de juarez','540' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (705,'MuebleriaFCFM6','Casa bella','Casa de juarez','550' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (706,'MuebleriaFCFM7','Casa bella','Casa de juarez','560' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (707,'MuebleriaFCFM8','Casa bella','Casa de juarez','570' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (708,'MuebleriaFCFM9','Casa bella','Casa de juarez','580' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (709,'MuebleriaFCFM10','Casa bella','Casa de juarez','590' )
+insert into Sucursal (negocio_id,Nombre_negocio,colonia,calle,numero_casa)values (710,'MuebleriaFCFM11','Casa bella','Casa de juarez','600' )
+
+
+Select * from ticket 
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(1,800,1000,'2000','25/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(2,800,1001,'2001','26/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(3,800,1002,'2002','27/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(4,800,1003,'2003','28/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(5,800,1004,'2004','29/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(6,800,1005,'2005','30/04/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(7,800,1006,'2006','01/05/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(8,800,1007,'2007','02/05/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(9,800,1008,'2008','03/05/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(10,800,1009,'2009','04/05/2019','Casa bella','Casa de juarez','505')
+insert into ticket (ticket_id,negocio_id,empleado_id,producto_comprado,fecha_compra,colonia,calle,numero_casa)values(11,800,1010,'2010','05/05/2019','Casa bella','Casa de juarez','505')
+
+
+update AlmacenSalas set costo = 36500 where id_Salas = 500
+update AlmacenSalas set costo = 37600 where id_Salas = 501
+update AlmacenSalas set costo = 46000 where id_Salas = 502
+update AlmacenSalas set costo = 24456 where id_Salas = 503
+update AlmacenSalas set costo = 45678 where id_Salas = 504
+update AlmacenSalas set costo = 37890 where id_Salas = 505
+update AlmacenSalas set costo = 41234 where id_Salas = 506
+update AlmacenSalas set costo = 56789 where id_Salas = 507
+update AlmacenSalas set costo = 34987 where id_Salas = 508
+update AlmacenSalas set costo = 35786 where id_Salas = 509
+
+delete from Empleados where Id_Empleado = 612
+delete from Empleados where Id_Empleado = 613
+delete from Empleados where Id_Empleado = 614
+delete from Empleados where Id_Empleado = 615
+delete from Empleados where Id_Empleado = 616
+delete from Empleados where Id_Empleado = 617
+delete from Empleados where Id_Empleado = 618
+delete from Empleados where Id_Empleado = 619
+delete from Empleados where Id_Empleado = 620
+delete from Empleados where Id_Empleado = 621
+
+
+
+
+
+
+
+
+
